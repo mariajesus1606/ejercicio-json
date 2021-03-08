1fc8b713efafcf850a61c1b2124c9c58aa1f022a
Maria Jesus <bpmariajesus20@gmail.com>
2021-03-08 20:36:56 +0100

    He modificado el fichero funciones.py y el fichero programa.py

--- funciones.py
+++ funciones.py
@@ -1,1 +1,97 @@
+import json
+
+#Funcion: Listar información: Listar los hoteles que tengan 4 estrellas (****)
+def hoteles_cuatro_estrellas(datos):
+    lista = []
+    for dato in datos["resources"]:
+        if dato["dc:title"][0:5] == "**** ":
+            lista.append(dato["dc:title"])
+    return lista
+
+
+#Funcion: Contar información: Contar los hoteles que existen, ordenados por estrellas, es decir, mostrara el numero de hoteles que hay con 5 estrellas, con 4 estrellas, etc...
+#def contar_hoteles(datos):
+#    lista = []
+#    una_estrella = 0 
+#    dos_estrellas = 0
+#    tres_estrellas = 0
+#    cuatro_estrellas = 0
+#    cinco_estrellas = 0
+#    for dato in datos["resources"]:
+#        if dato["dc:title"].count("*") == 5:
+#            cinco_estrellas = cinco_estrellas + 1
+#            lista.append(int((cinco_estrellas))
+#        if dato["dc:title"].count("*") == 4:
+#            cuatro_estrellas = cuatro_estrellas + 1
+#            lista.append(int((cuatro_estrellas))
+#        if dato["dc:title"].count("*") == 3:
+#            tres_estrellas = tres_estrellas + 1
+#            lista.append(int((tres_estrellas))
+#        if dato["dc:title"].count("*") == 2:
+#            dos_estrellas = dos_estrellas + 1
+#            lista.append(int((dos_estrellas))
+#        if dato["dc:title"].count("*") == 1:
+#            una_estrella = una_estrella + 1
+#            lista.append(int(una_estrella))
+#    return lista 
+
+
+#Funcion: Buscar o filtrar información: Pide por teclado una categoria de precio por teclado (Economico, Medio, Alto) y te muestra los hoteles que tienen esa categoria de precio.
+#def categoria_por_precio(datos,categoria):
+#    caros = []
+#    normales = []
+#    baratos = []
+#    for dato in datos["resources"]:
+#	    if dato["lpgc:precio"] == "alto":
+#		    caros.append(dato["dc:title"])
+#        if dato["lpgc:precio"] == "medio":
+#		    normales.append(dato["dc:title"])
+#        if dato["lpgc:precio"] == "Económico":
+#		    baratos.append(dato["dc:title"])
+#    lista = [caros,normales,baratos]
+#    return lista
+
+
+#Funcion: Buscar información relacionada: Pide por teclado los dos ultimos numeros del telefono y nos mostrara la lista de telefonos que coinciden junto con el nombre del hotel.
+def buscar_informacion(datos,num):
+    lista = []
+    for dato in datos["resources"]:
+	    if dato["lpgc:telefono"][-2:] == num:
+		    lista.append("Nombre Hotel: " + dato["dc:title"] + " | Numero Telefono: " + dato["lpgc:telefono"]) 
+    return lista 
+
+#Funcion: Ejercicio libre: Pide por teclado el nombre de un hotel sin estrellas (ya que tenemos que tener en cuenta que los nombres de los hoteles de nuestro fichero empiezan con "*" que son las estrellas )y nos devuelve su pagina web.
+def ejercicio_libre(datos):
+    hoteles = {}
+    for d in datos["resources"]:
+	    if d["dc:title"].count("*") == 5:
+		    d["dc:title"] = d["dc:title"].lstrip("***** ")
+		    key = d["dc:title"] 
+		    value = d["lpgc:web"]
+		    hoteles[key] = value
+	    elif d["dc:title"].count("*") == 4:
+		    d["dc:title"] = d["dc:title"].lstrip("**** ")
+		    key = d["dc:title"] 
+		    value = d["lpgc:web"]
+		    hoteles[key] = value
+	    elif d["dc:title"].count("*") == 3:
+		    d["dc:title"] = d["dc:title"].lstrip("*** ")
+		    key = d["dc:title"] 
+		    value = d["lpgc:web"]
+		    hoteles[key] = value
+	    elif d["dc:title"].count("*") == 2:
+		    d["dc:title"] = d["dc:title"].lstrip("** ")
+		    key = d["dc:title"] 
+		    value = d["lpgc:web"]
+		    hoteles[key] = value
+	    elif d["dc:title"].count("*") == 1:
+		    d["dc:title"] = d["dc:title"].lstrip("* ")
+		    key = d["dc:title"] 
+		    value = d["lpgc:web"]
+		    hoteles[key] = value
+	    else:
+		    key = d["dc:title"] 
+		    value = d["lpgc:web"]
+		    hoteles[key] = value
+    return 
 
--- programa.py
+++ programa.py
@@ -1,1 +1,86 @@
+from funciones import *
+
+import json
+
+with open ('info-hoteles.json','r') as fichero:
+    datos = json.load(fichero)
+
+print("--------")
+print("- MENU -")
+print("--------")
+menu = '''1. Listar información
+2. Contar información
+3. Buscar o filtrar información
+4. Buscar información relacionada
+5. Ejercicio libre
+6. Salir'''
+
+print(menu)
+
+opcion = int(input("Opción: "))
+while opcion <1 or opcion >6:
+    print("Opcion Incorrecta")
+    opcion = int(input("Opción: "))
+
+while opcion !=6:
+    #Listar información: Listar los hoteles que tengan 4 estrellas (****)
+    if opcion == 1:
+        print("HOTELES 4 ESTRELLAS: ")
+        print("---------------------")
+        for elem in hoteles_cuatro_estrellas(datos):
+            print(elem)
+    #Contar información: Contar los hoteles que existen, ordenados por estrellas, es decir, mostrara el numero de hoteles que hay con 5 estrellas, con 4 estrellas, etc...
+    #elif opcion == 2:
+        #for elem in contar_hoteles(datos):
+            #print("Hoteles de 5 estrellas: ",(elem[4]))
+            #print("Hoteles de 4 estrellas: ",(elem[3]))
+            #print("Hoteles de 3 estrellas: ",(elem[2]))
+            #print("Hoteles de 2 estrellas: ",(elem[1]))
+            #print("Hoteles de 1 estrellas: ",(elem[0]))
+    ##Buscar o filtrar información: Pide por teclado una categoria de precio por teclado (Economico, Medio, Alto) y te muestra los hoteles que tienen esa categoria de precio.
+    #elif opcion == 3:
+    #    print("HOTELES POR CATEGORIAS: ")
+    #    print("------------------------")
+    #    lista = ['caro','normal','barato']
+    #    print("Categorias: ")
+    #    for i in lista:
+    #        print(i)
+    #    categoria = input("Introduce la categoria: ")
+    #    for elem in categoria_por_precio(datos,categoria):
+    #        if categoria == "caro":
+    #            for c in elem[0]:
+    #                print(c)
+    #        if categoria == "normal":
+    #            for n in elem[1]:
+    #                print(n)
+    #        if categoria == "barato":
+    #            for b in elem[2]:
+    #                print(b)
+    #Buscar información relacionada: Pide por teclado los dos ultimos numeros del telefono y nos mostrara la lista de telefonos que coinciden junto con el nombre del hotel.
+    elif opcion == 4:
+        print("NUMEROS DE TELEFONO: ")
+        #for dato in datos["resources"]:
+        #    for elem in dato:
+        #Mostrar telefonos
+        num = input("Ultimos dos digitos de numero de telefono:" )
+        print("-------------------------------------------------")
+        for i in buscar_informacion(datos,num):
+            print(i)
+    #Ejercicio libre: Pide por teclado el nombre de un hotel sin estrellas (ya que tenemos que tener en cuenta que los nombres de los hoteles de nuestro fichero empiezan con "*" que son las estrellas )y nos devuelve su pagina web.
+    elif opcion == 5:
+        var = unicode(raw_input("Nombre del hotel o que empiece por tal cadena: "),'utf-8').title()
+        for k, v in ejercicio_libre(datos).items():
+	        if k.startswith(var) and v == "":
+		        print "La url del sitio web del hotel " + k + " --> " + "No esta disponible"
+	        elif k.startswith(var) and v != "":
+		        print "La url del sitio web del hotel " + k + " --> " + v
+    print("--------")
+    print("- MENU -")
+    print("--------")
+    print(menu)
+    opcion = int(input("Opción: "))
+    while opcion <1 or opcion >6:
+        print("Opcion Incorrecta")
+        opcion = int(input("Opción: "))
+        
 
